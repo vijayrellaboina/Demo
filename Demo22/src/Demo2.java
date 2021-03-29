09d4dfe47f308d509442d0c7c725bee2a98aede2
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-29 23:58:19 +0530

    conflit will occur

--- Demo22/src/Demo2.java
+++ Demo22/src/Demo2.java
@@ -7,6 +7,7 @@ System.out.println("second line");
 System.out.println("third line");
 System.out.println("fourth line");
 System.out.println("fifth line");
+System.out.println("sixth line");
 	}
 
 }
