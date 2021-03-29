5139278650b654eebe54ecbc329d8470de7a8215
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-29 20:54:33 +0530

    Perform PULL operation in your eclipse

---
+++ Demo22/src/Acharya.java
@@ -0,0 +1,10 @@
+
+public class Acharya {
+
+	public static void main(String[] args) {
+     String Hero="MEGA STAR CHIRANJEEVI";
+     String Heroine= "KAJAL";
+     String Director="KORATALA SIVA";
+	}
+
+}
---
+++ Demo22/src/VakeelSab.java
@@ -0,0 +1,10 @@
+
+public class VakeelSab {
+
+	public static void main(String[] args) {
+      String Hero="PAWAN KALYAN";
+      String Heroine = "SHRUTHI HASSAN";
+      
+	}
+
+}
